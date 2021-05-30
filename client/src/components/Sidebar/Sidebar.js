512518ca9c562b3679f1240d6b08a48f47b178f5
Shrirang <deshmukhshrirang01@gmail.com>
2021-05-30 12:03:26 +0530

    Small Correction

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -19,7 +19,6 @@ import Typography from "@material-ui/core/Typography";
 // @material-ui/icons components
 import Clear from "@material-ui/icons/Clear";
 import MenuIcon from "@material-ui/icons/Menu";
-import Notification from "./../Navbars/Notification";
 // core components
 import componentStyles from "assets/theme/components/sidebar.js";
 
